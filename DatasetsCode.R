9b384a2028166e614bd3ad184df3d531e820c916
amaroda <128505008+amaroda@users.noreply.github.com>
2023-04-28 21:21:30 +0200

    Add files via upload

--- DatasetsCode.R
+++ DatasetsCode.R
@@ -16,9 +16,9 @@ library(ggfortify)
 # Op. 38
 # 20 recordings
 
-#setwd("C:/Users/samte/OneDrive/Desktop/TFM Clustering of musical patterns/Data/Op. 38i")
+setwd("C:/Users/samte/OneDrive/Desktop/TFM Clustering of musical patterns/Data/Op. 38i")
 #setwd("C:/Users/samte/OneDrive/Desktop/TFM Clustering of musical patterns/Data/Op. 38ii")
-setwd("C:/Users/samte/OneDrive/Desktop/TFM Clustering of musical patterns/Data/Op. 38iii")
+#setwd("C:/Users/samte/OneDrive/Desktop/TFM Clustering of musical patterns/Data/Op. 38iii")
 
 mydir = as.vector(c("C&H(58)","DP&B","F&B","F&vdP","G&G","G&Gr","G&Gu",
                     "I&H","M&A","M&G","M&L","M&P","P&N","P&R(36)","P&R(66)",
@@ -37,7 +37,7 @@ for (i in 1:length(mydir)){
   miname <- myname[i]
   myfiles = list.files(path=midir, pattern="*.csv", full.names=TRUE)
   
-  for (j in c(2)){
+  for (j in c(4,5)){
     type <- mytype[j]
     assign(paste0(miname,type), read.csv(myfiles[j],sep=";")) 
     
@@ -410,7 +410,7 @@ SS <- crotchet_fun(SyScrotchets,SS)
 
 crotchets38iii <- rbind(CH,DPB,FB,FvdP,GG,  GGr,GGu,IH,MA,MG,  ML,MP,PR36,PR66,PS,  PN,PV,RS,SB,SS)
 
-# Tidying the crotchet 38i dataset
+# Tidying the crotchet 38iii dataset
 
 crotchets38iii$beat_1 <- crotchets38iii$V1
 crotchets38iii$beat_2 <- crotchets38iii$V2
@@ -460,7 +460,7 @@ SS <- crotchet_fun99i(SyScrotchets,SS)
 
 crotchets99i <- rbind(CH,DPB,FB,GG,GGu,  IH,MA,MG,ML,PR66,  PN,PV,RS,SB,SS)
 
-# Tidying the crotchet 38i dataset
+# Tidying the crotchet 99i dataset
 
 crotchets99i$beat_1 <- crotchets99i$V1
 crotchets99i$beat_2 <- crotchets99i$V2
@@ -506,7 +506,8 @@ SS <- semiquaver_fun99ii(SySsemiquavers,SS)
 
 semiquavers99ii <- rbind(CH,DPB,FB,GG,GGu,  IH,MA,MG,ML,PR66,  PN,PV,RS,SB,SS)
 
-# Tidying the 38ii quaver dataset
+
+# Tidying the 99ii semiquaver dataset
 
 semiquavers99ii$beat_1 <- semiquavers99ii$V1
 semiquavers99ii$beat_2 <- semiquavers99ii$V2
@@ -546,3 +547,289 @@ semiquavers99ii$V16<-NULL
 semiquavers99ii$V17<-NULL
 
 semiquavers99ii$bar <- as.factor(semiquavers99ii$bar)
+
+write.csv(crotchets99ii, "C:/Users/samte/OneDrive/Desktop/crotchets99ii.csv", row.names=FALSE)
+
+### columns of performers
+
+vc38i <- matrix(,nrow=2160,ncol=20)
+var <-0
+for (k in c("CH","DPB" ,"FB" ,"FvdP" ,"GG" ,"GGr" ,"GGu" ,"IH" ,"MA","MG",
+            "ML" ,"MP" ,"PN" ,"PR36" ,"PR66" ,"PS" ,"PV" ,"RS" ,"SB" ,"SS")){
+  var=var+1
+  dataframe <- crotchets38i[crotchets38i$performer == k,]
+  if (dim(dataframe)[1]>300){
+    dataframe <- dataframe[91:370,]
+  }
+  
+  for(j in 1:270){
+    for (i in 1:8){
+      vc38i[i+8*(j-1),var]<-dataframe[j,i+1]
+      
+    }
+  }
+}
+
+
+vc38i <- as.data.frame(vc38i)
+
+vc38i$CH <- vc38i$V1
+vc38i$DPB <- vc38i$V2
+vc38i$FB <- vc38i$V3
+vc38i$FvdP <- vc38i$V4
+vc38i$GG <- vc38i$V5
+
+vc38i$GGr <- vc38i$V6
+vc38i$GGu <- vc38i$V7
+vc38i$IH <- vc38i$V8
+vc38i$MA <- vc38i$V9
+vc38i$MG <- vc38i$V10
+
+vc38i$ML <- vc38i$V11
+vc38i$MP <- vc38i$V12
+vc38i$PN <- vc38i$V13
+vc38i$PR36 <- vc38i$V14
+vc38i$PR66 <- vc38i$V15
+
+vc38i$PS <- vc38i$V16
+vc38i$PV <- vc38i$V17
+vc38i$RS <- vc38i$V18
+vc38i$SB <- vc38i$V19
+vc38i$SS <- vc38i$V20
+
+vc38i$V1 <- NULL
+vc38i$V2 <- NULL
+vc38i$V3 <- NULL
+vc38i$V4 <- NULL
+vc38i$V5 <- NULL
+
+vc38i$V6 <- NULL
+vc38i$V7 <- NULL
+vc38i$V8 <- NULL
+vc38i$V9 <- NULL
+vc38i$V10 <- NULL
+
+vc38i$V11 <- NULL
+vc38i$V12 <- NULL
+vc38i$V13 <- NULL
+vc38i$V14 <- NULL
+vc38i$V15 <- NULL
+
+vc38i$V16 <- NULL
+vc38i$V17 <- NULL
+vc38i$V18 <- NULL
+vc38i$V19 <- NULL
+vc38i$V20 <- NULL
+
+write.csv(vc38i, "C:/Users/samte/OneDrive/Desktop/vc38i.csv", row.names=FALSE)
+
+### rows of performers
+
+tc38i <- t(vc38i)
+tc38i <- as.data.frame(tc38i)
+tc38i$performers <- c("CH","DPB" ,"FB" ,"FvdP" ,"GG" ,"GGr" ,"GGu" ,"IH" ,"MA","MG",
+                      "ML" ,"MP" ,"PN" ,"PR36" ,"PR66" ,"PS" ,"PV" ,"RS" ,"SB" ,"SS")
+
+write.csv(tc38i, "C:/Users/samte/OneDrive/Desktop/tc38i.csv", row.names=FALSE)
+
+tscaled <- scale(tc38i[,1:2160])
+tscaled <- as.data.frame(tscaled)
+tscaled$performers <- tc38i$performers
+
+write.csv(tscaled, "C:/Users/samte/OneDrive/Desktop/tscaled.csv", row.names=FALSE)
+
+### unused for now
+
+t<-t(tscaled)
+t<- as.data.frame(t)
+t<-t[1:2160,]
+
+t$CH <- as.numeric(t$CH)
+t$DPB <- as.numeric(t$DPB)
+t$FB <- as.numeric(t$FB)
+t$FvdP <- as.numeric(t$FvdP)
+t$GG <- as.numeric(t$GG)
+
+t$GGr <- as.numeric(t$GGr)
+t$GGu <- as.numeric(t$GGu)
+t$IH <- as.numeric(t$IH)
+t$MA <- as.numeric(t$MA)
+t$MG <- as.numeric(t$MG)
+
+t$ML <- as.numeric(t$ML)
+t$MP <- as.numeric(t$MP)
+t$PN <- as.numeric(t$PN)
+t$PR36 <- as.numeric(t$PR36)
+t$PR66 <- as.numeric(t$PR66)
+
+t$PS <- as.numeric(t$PS)
+t$PV <- as.numeric(t$PV)
+t$RS <- as.numeric(t$RS)
+t$SB <- as.numeric(t$SB)
+t$SS <- as.numeric(t$SS)
+
+cor(t)
+
+### beats and intensity
+
+v <- numeric(1080)
+for (i in seq(1,540,by=2)){
+  v[1+2*(i-1)] <- 1+4*(i-1)
+  v[2+2*(i-1)] <- 2+4*(i-1)
+  v[3+2*(i-1)] <- 3+4*(i-1)
+  v[4+2*(i-1)] <- 4+4*(i-1)
+  
+}
+
+tbeats <- tc38i[,v]
+tintensity <- tc38i[,-v]
+tbeats$performers <- tintensity$performers
+
+write.csv(tbeats, "C:/Users/samte/OneDrive/Desktop/tbeats.csv", row.names=FALSE)
+write.csv(tintensity, "C:/Users/samte/OneDrive/Desktop/tintensity.csv", row.names=FALSE)
+
+### notes cello
+
+notes.cello38i<-rbind(CyH58notes.cello[310:1248,]$Duration..s.,DPyBnotes.cello$Duration..s.,FyBnotes.cello$Duration..s.,
+                      FyvdPnotes.cello$Duration..s.,GyGnotes.cello[310:1248,]$Duration..s.,GyGrnotes.cello[310:1248,]$Duration..s.,
+                      GyGunotes.cello[310:1248,]$Duration..s.,IyHnotes.cello[310:1248,]$Duration..s.,MyAnotes.cello$Duration..s.,
+                      MyGnotes.cello[310:1248,]$Duration..s.,MyLnotes.cello[310:1248,]$Duration..s.,MyPnotes.cello[310:1248,]$Duration..s.,
+                      PyNnotes.cello[310:1248,]$Duration..s.,PyR36notes.cello$Duration..s.,PyR66notes.cello$Duration..s.,
+                      PySnotes.cello$Duration..s.,PyVnotes.cello[310:1248,]$Duration..s.,RySnotes.cello[310:1248,]$Duration..s.,SyBnotes.cello$Duration..s.,SySnotes.cello[310:1248,]$Duration..s.)
+# error en PN, en algun momento faltan dos notas (633,634)
+# revisar IH, PR66
+notes.cello38i <- as.data.frame(notes.cello38i)
+notes.cello38i$V939 <- NULL
+notes.cello38i$V938 <- NULL
+notes.cello38i$performers <- c("CH","DPB" ,"FB" ,"FvdP" ,"GG" ,"GGr" ,"GGu" ,"IH" ,"MA","MG",
+                               "ML" ,"MP" ,"PN" ,"PR36" ,"PR66" ,"PS" ,"PV" ,"RS" ,"SB" ,"SS")
+
+write.csv(notes.cello38i, "C:/Users/samte/OneDrive/Desktop/notes.cello38i.csv", row.names=FALSE)
+
+### transposed notes
+
+tcello<-t(notes.cello38i[,1:937])
+tcello <- as.data.frame(tcello)
+
+tcello$CH <- tcello$V1
+tcello$DPB <- tcello$V2
+tcello$FB <- tcello$V3
+tcello$FvdP <- tcello$V4
+tcello$GG <- tcello$V5
+
+tcello$GGr <- tcello$V6
+tcello$GGu <- tcello$V7
+tcello$IH <- tcello$V8
+tcello$MA <- tcello$V9
+tcello$MG <- tcello$V10
+
+tcello$ML <- tcello$V11
+tcello$MP <- tcello$V12
+tcello$PN <- tcello$V13
+tcello$PR36 <- tcello$V14
+tcello$PR66 <- tcello$V15
+
+tcello$PS <- tcello$V16
+tcello$PV <- tcello$V17
+tcello$RS <- tcello$V18
+tcello$SB <- tcello$V19
+tcello$SS <- tcello$V20
+
+tcello$V1 <- NULL
+tcello$V2 <- NULL
+tcello$V3 <- NULL
+tcello$V4 <- NULL
+tcello$V5 <- NULL
+
+tcello$V6 <- NULL
+tcello$V7 <- NULL
+tcello$V8 <- NULL
+tcello$V9 <- NULL
+tcello$V10 <- NULL
+
+tcello$V11 <- NULL
+tcello$V12 <- NULL
+tcello$V13 <- NULL
+tcello$V14 <- NULL
+tcello$V15 <- NULL
+
+tcello$V16 <- NULL
+tcello$V17 <- NULL
+tcello$V18 <- NULL
+tcello$V19 <- NULL
+tcello$V20 <- NULL
+
+cor(tcello)
+
+write.csv(tcello, "C:/Users/samte/OneDrive/Desktop/tcello.csv", row.names=FALSE)
+
+### notes piano
+
+notes.piano38i<-rbind(CyH58notes.piano[526:2221,]$Duration..s.,DPyBnotes.piano$Duration..s.,FyBnotes.piano$Duration..s.,
+                      FyvdPnotes.piano$Duration..s.,GyGnotes.piano[526:2221,]$Duration..s.,GyGrnotes.piano[526:2221,]$Duration..s.,
+                      GyGunotes.piano[526:2221,]$Duration..s.,IyHnotes.piano[526:2221,]$Duration..s.,MyAnotes.piano$Duration..s.,
+                      MyGnotes.piano[526:2221,]$Duration..s.,MyLnotes.piano[526:2221,]$Duration..s.,MyPnotes.piano[526:2221,]$Duration..s.,
+                      PyNnotes.piano[526:2221,]$Duration..s.,PyR36notes.piano$Duration..s.,PyR66notes.piano$Duration..s.,
+                      PySnotes.piano$Duration..s.,PyVnotes.piano[526:2221,]$Duration..s.,RySnotes.piano[526:2221,]$Duration..s.,SyBnotes.piano$Duration..s.,SySnotes.piano[526:2221,]$Duration..s.)
+# revisar IH
+notes.piano38i <- as.data.frame(notes.piano38i)
+notes.piano38i$V1696 <- NULL
+notes.piano38i$performers <- c("CH","DPB" ,"FB" ,"FvdP" ,"GG" ,"GGr" ,"GGu" ,"IH" ,"MA","MG",
+                               "ML" ,"MP" ,"PN" ,"PR36" ,"PR66" ,"PS" ,"PV" ,"RS" ,"SB" ,"SS")
+
+write.csv(notes.piano38i, "C:/Users/samte/OneDrive/Desktop/notes.piano38i.csv", row.names=FALSE)
+
+### transposed notes
+
+tpiano<-t(notes.piano38i[,1:1695])
+tpiano <- as.data.frame(tpiano)
+
+tpiano$CH <- tpiano$V1
+tpiano$DPB <- tpiano$V2
+tpiano$FB <- tpiano$V3
+tpiano$FvdP <- tpiano$V4
+tpiano$GG <- tpiano$V5
+
+tpiano$GGr <- tpiano$V6
+tpiano$GGu <- tpiano$V7
+tpiano$IH <- tpiano$V8
+tpiano$MA <- tpiano$V9
+tpiano$MG <- tpiano$V10
+
+tpiano$ML <- tpiano$V11
+tpiano$MP <- tpiano$V12
+tpiano$PN <- tpiano$V13
+tpiano$PR36 <- tpiano$V14
+tpiano$PR66 <- tpiano$V15
+
+tpiano$PS <- tpiano$V16
+tpiano$PV <- tpiano$V17
+tpiano$RS <- tpiano$V18
+tpiano$SB <- tpiano$V19
+tpiano$SS <- tpiano$V20
+
+tpiano$V1 <- NULL
+tpiano$V2 <- NULL
+tpiano$V3 <- NULL
+tpiano$V4 <- NULL
+tpiano$V5 <- NULL
+
+tpiano$V6 <- NULL
+tpiano$V7 <- NULL
+tpiano$V8 <- NULL
+tpiano$V9 <- NULL
+tpiano$V10 <- NULL
+
+tpiano$V11 <- NULL
+tpiano$V12 <- NULL
+tpiano$V13 <- NULL
+tpiano$V14 <- NULL
+tpiano$V15 <- NULL
+
+tpiano$V16 <- NULL
+tpiano$V17 <- NULL
+tpiano$V18 <- NULL
+tpiano$V19 <- NULL
+tpiano$V20 <- NULL
+
+write.csv(tpiano, "C:/Users/samte/OneDrive/Desktop/tpiano.csv", row.names=FALSE)
